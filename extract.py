a572dfbe5b23bd23de39ecf0c7164fa5168db171
egehanyorulmaz <egehanyorulmaz@sabanciuniv.edu>
2023-01-15 23:03:55 -0600

    adds limit parameter to download bulk data

--- extract.py
+++ extract.py
@@ -2,8 +2,10 @@ import requests
 import pandas as pd
 import json
 
-url = "https://data.cityofchicago.org/resource/4ijn-s7e5.json"
+url = "https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=5000000"
 
 response = requests.get(url)
 df = pd.DataFrame(response.json())
+
 df.to_csv('Food_Inspections.csv', index=False)
+print("The food inspections data is successfully downloaded.")
